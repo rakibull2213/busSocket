1410dfb958384cd678da14eedbbaec5f16cbfbd6
rakibull2213 <121360460+rakibull2213@users.noreply.github.com>
2024-02-02 18:06:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Server = require('http').createServer();
-const io = require('socket.io')?.(Server);
+const io = require('socket.io')(Server); // Remove the optional chaining operator
 
 const bus = io.of('/bus');
 const user = io.of('/user');
+
 bus.on('connection', (socket) => {
     socket.on("bus_moved", (data) => {
         if (data?.long && data?.lat) {
             user.emit("C_bus_moved", {long: data.long, lat: data.lat});
         }
-    })
+    });
 });
+
 Server.on('request', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Bus is working\n');
